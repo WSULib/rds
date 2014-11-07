3b24dcf2757555a7533f7fdd7495a0f33f93ed72
Cole Hudson <cole.hudson@wayne.edu>
2014-11-07 11:00:34 -0500

    updated links on data journals page

--- data_journals.php
+++ data_journals.php
@@ -20,142 +20,142 @@
             <row class="row">
                 <h2><a name="general_sci">General science journals</a></h2><br/>
                 <ul>
-                <li><a href="">Dataset Papers in Science</a></li>
-                <li><a href="">F1000 Research</a></li>
-                <li><a href="">Gigascience</a></li>
-                <li><a href="">Journal of Open Research Software</a></li>
-                <li><a href="">PLoS ONE</a></li>
-                <li><a href="">Scientific Data</a></li>
-                <li><a href="">SpringerPlus</a></li>
+                    <li><a href='http://www.hindawi.com/journals/dpis/'>Dataset Papers in Science</a></li>
+                    <li><a href='http://f1000research.com/'>F1000 Research</a></li>
+                    <li><a href='http://www.gigasciencejournal.com/'>Gigascience</a></li>
+                    <li><a href='http://openresearchsoftware.metajnl.com/'>Journal of Open Research Software</a></li>
+                    <li><a href='http://www.plosone.org/'>PLoS ONE</a></li>
+                    <li><a href='http://www.nature.com/sdata/'>Scientific Data</a></li>
+                    <li><a href='http://www.springerplus.com/'>SpringerPlus</a></li>
                 </ul>
             </row>
             <row class="row">
                 <h2><a name="agriculture">Agriculture journals</a></h2><br/>
                 <ul>
-                <li><a href="">Open Data Journal for Agricultural Research</a></li>
+                    <li><a href='http://www.odjar.org/'>Open Data Journal for Agricultural Research</a></li>
                 </ul>
             </row>
             <row class="row">
                 <h2><a name="archeology">Archeology journals</a></h2><br/>
                 <ul>
-                <li><a href="">Internet Archaeology</a></li>
-                <li><a href="">Journal of Open Archaeology Data</a></li>
+                    <li><a href='http://intarch.ac.uk/'>Internet Archaeology</a></li>
+                    <li><a href='http://openarchaeologydata.metajnl.com/'>Journal of Open Archaeology Data</a></li>
                 </ul>
             </row>
             <row class="row">
                 <h2><a name="astronomy">Astronomy journals</a></h2><br/>
                 <ul>
-                <li><a href="">The Journal of Astronomical Data</a></li>
+                    <li><a href='http://www.vub.ac.be/STER/JAD/jad.htm'>The Journal of Astronomical Data</a></li>
                 </ul>
             </row>
             <row class="row">
                 <h2><a name="biomedical">Biomedical journals</a></h2><br/>
                 <ul>
-                <li><a href="">Biomedical Data Journal</a></li>
-                <li><a href="">BMC Bioinformatics</a></li>
-                <li><a href="">BMC Biophysics</a></li>
-                <li><a href="">BMC Biotechnology</a></li>
-                <li><a href="">BMC Cancer</a></li>
-                <li><a href="">BMC Cell Biology</a></li>
-                <li><a href="">BMC Complementary & Alternative Medicine</a></li>
-                <li><a href="">BMC Dermatology</a></li>
-                <li><a href="">BMC Developmental Biology</a></li>
-                <li><a href="">BMC Gastroenterology</a></li>
-                <li><a href="">BMC Genetics</a></li>
-                <li><a href="">BMC Genomics</a></li>
-                <li><a href="">BMC Geriatrics</a></li>
-                <li><a href="">BMC Immunology</a></li>
-                <li><a href="">BMC Infectious Diseases</a></li>
-                <li><a href="">BMC Medical Education</a></li>
-                <li><a href="">BMC Medical Genetics</a></li>
-                <li><a href="">BMC Medical Genomics</a></li>
-                <li><a href="">BMC Medical Imaging</a></li>
-                <li><a href="">BMC Medical Informatics & Decision Making</a></li>
-                <li><a href="">BMC Medical Research Methodology</a></li>
-                <li><a href="">BMC Microbiology</a></li>
-                <li><a href="">BMC Molecular Biology</a></li>
-                <li><a href="">BMC Musculoskeletal Disorders</a></li>
-                <li><a href="">BMC Nephrology</a></li>
-                <li><a href="">BMC Neuroscience</a></li>
-                <li><a href="">BMC Ophthalmology</a></li>
-                <li><a href="">BMC Palliative Care</a></li>
-                <li><a href="">BMC Pharmacology & Toxicology</a></li>
-                <li><a href="">BMC Pregnancy & Childbirth</a></li>
-                <li><a href="">BMC Psychiatry</a></li>
-                <li><a href="">BMC Pulmonary Medicine</a></li>
-                <li><a href="">BMC Structural Biology</a></li>
-                <li><a href="">BMC Systems Biology</a></li>
-                <li><a href="">BMC Research Notes</a></li>
-                <li><a href="">BMC Urology</a></li>
-                <li><a href="">Genomics Data</a></li>
-                <li><a href="">Neuroinformatics</a></li>
-                <li><a href="">Standards in Genomic Sciences</a></li>
+                    <li><a href='http://biomed-data.eu/'>Biomedical Data Journal</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcbioinformatics'>BMC Bioinformatics</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcbiophys'>BMC Biophysics</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcbiotechnol'>BMC Biotechnology</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmccancer'>BMC Cancer</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmccellbiol'>BMC Cell Biology</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmccomplementalternmed'>BMC Complementary &amp; Alternative Medicine</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcdermatol'>BMC Dermatology</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcdevbiol'>BMC Developmental Biology</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcgastroenterol'>BMC Gastroenterology</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcgenet'>BMC Genetics</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcgenomics'>BMC Genomics</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcgeriatr'>BMC Geriatrics</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcimmunol'>BMC Immunology</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcinfectdis'>BMC Infectious Diseases</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcmededuc'>BMC Medical Education</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcmedgenet'>BMC Medical Genetics</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcmedgenomics'>BMC Medical Genomics</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcmedimaging'>BMC Medical Imaging</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcmedinformdecismak'>BMC Medical Informatics &amp; Decision Making</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcmedresmethodol'>BMC Medical Research Methodology</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcmicrobiol'>BMC Microbiology</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcmolbiol'>BMC Molecular Biology</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcmusculoskeletdisord'>BMC Musculoskeletal Disorders</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcnephrol'>BMC Nephrology</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcneurosci'>BMC Neuroscience</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcophthalmol'>BMC Ophthalmology</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcpalliatcare'>BMC Palliative Care</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcpharmacoltoxicol'>BMC Pharmacology &amp; Toxicology</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcpregnancychildbirth'>BMC Pregnancy &amp; Childbirth</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcpsychiatry'>BMC Psychiatry</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcpulmmed'>BMC Pulmonary Medicine</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcstructbiol'>BMC Structural Biology</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcsystbiol'>BMC Systems Biology</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcresnotes'>BMC Research Notes</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcurol'>BMC Urology</a></li>
+                    <li><a href='http://www.journals.elsevier.com/genomics-data/'>Genomics Data</a></li>
+                    <li><a href='http://www.springer.com/biomed/neuroscience/journal/12021'>Neuroinformatics</a></li>
+                    <li><a href='http://www.standardsingenomics.org/'>Standards in Genomic Sciences</a></li>
                 </ul>
             </row>
             <row class="row">
                 <h2><a name="chemistry">Chemistry journals</a></h2><br/>
                 <ul>
-                <li><a href="">Acta Crystallographica E: Structure Reports Online  </a></li>
-                <li><a href="">Journal of Chemical and Engineering Data</a></li>
-                <li><a href="">Journal of Physical and Chemical Reference Data</a></li>
+                    <li><a href='http://journals.iucr.org/e/'>Acta Crystallographica E: Structure Reports Online</a></li>
+                    <li><a href='http://pubs.acs.org/journal/jceaax'>Journal of Chemical and Engineering Data</a></li>
+                    <li><a href='http://scitation.aip.org/content/aip/journal/jpcrd'>Journal of Physical and Chemical Reference Data</a></li>
                 </ul>
             </row>
             <row class="row">
                 <h2><a name="ecology">Ecology and evolutionary biology journals</a></h2><br/>
                 <ul>
-                <li><a href="">Biodiversity Data Journal</a></li>
-                <li><a href="">BioRisk</a></li>
-                <li><a href="">BMC Ecology</a></li>
-                <li><a href="">BMC Evolutionary Biology</a></li>
-                <li><a href="">BMC Plant Biology</a></li>
-                <li><a href="">Ecological Research</a></li>
-                <li><a href="">Ecology</a></li>
-                <li><a href="">MycoKeys</a></li>
-                <li><a href="">Nature Conservation</a></li>
-                <li><a href="">NeoBiota</a></li>
-                <li><a href="">PhytoKeys</a></li>
-                <li><a href="">ZooKeys</a></li>
+                    <li><a href='http://biodiversitydatajournal.com/'>Biodiversity Data Journal</a></li>
+                    <li><a href='http://www.pensoft.net/journals/biorisk/'>BioRisk</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcecol'>BMC Ecology</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcevolbiol'>BMC Evolutionary Biology</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcplantbiol'>BMC Plant Biology</a></li>
+                    <li><a href='http://link.springer.com/journal/11284'>Ecological Research</a></li>
+                    <li><a href='http://www.esajournals.org/loi/ecol'>Ecology</a></li>
+                    <li><a href='http://www.pensoft.net/journals/mycokeys/'>MycoKeys</a></li>
+                    <li><a href='http://www.pensoft.net/journals/natureconservation/'>Nature Conservation</a></li>
+                    <li><a href='http://www.pensoft.net/journals/neobiota/'>NeoBiota</a></li>
+                    <li><a href='http://www.pensoft.net/journals/phytokeys'>PhytoKeys</a></li>
+                    <li><a href='http://www.pensoft.net/journals/zookeys/'>ZooKeys</a></li>
                 </ul>
             </row>
             <row class="row">
                 <h2><a name="geoscience">Geoscience journals</a></h2><br/>
                 <ul>
-                <li><a href="">Earth System Science Data</a></li>
-                <li><a href="">Geochemistry, Geophysics, Geosystems</a></li>
-                <li><a href="">Geoscience Data Journal</a></li>
-                <li><a href="">Geoscientific Model Development</a></li>
+                    <li><a href='http://www.earth-system-science-data.net/'>Earth System Science Data</a></li>
+                    <li><a href='http://agupubs.onlinelibrary.wiley.com/agu/journal/10.1002/(ISSN)1525-2027/'>Geochemistry, Geophysics, Geosystems</a></li>
+                    <li><a href='http://www.geosciencedata.com/'>Geoscience Data Journal</a></li>
+                    <li><a href='http://www.geoscientific-model-development.net/'>Geoscientific Model Development</a></li>
                 </ul>
             </row>
             <row class="row">
                 <h2><a name="physics">Physics journals</a></h2><br/>
                 <ul>
-                <li><a href="">Atomic Data and Nuclear Data Tables</a></li>
-                <li><a href="">Nuclear Data Sheets</a></li>
+                    <li><a href='http://www.journals.elsevier.com/atomic-data-and-nuclear-data-tables/'>Atomic Data and Nuclear Data Tables</a></li>
+                    <li><a href='http://www.journals.elsevier.com/nuclear-data-sheets/'>Nuclear Data Sheets</a></li>
                 </ul>
             </row>
             <row class="row">
                 <h2><a name="psychology">Psychology journals</a></h2><br/>
                 <ul>
-                <li><a href="">Journal of Open Psychology Data</a></li>
+                    <li><a href='http://openpsychologydata.metajnl.com/'>Journal of Open Psychology Data</a></li>
                 </ul>
             </row>
             <row class="row">
                 <h2><a name="public_health">Public health journals</a></h2><br/>
                 <ul>
-                <li><a href="">BMC Public Health</a></li>
-                <li><a href="">Open Health Data</a></li>
+                    <li><a href='http://www.biomedcentral.com/bmcpublichealth'>BMC Public Health</a></li>
+                    <li><a href='http://openhealthdata.metajnl.com/'>Open Health Data</a></li>
                 </ul>
             </row>
             <row class="row">
                 <h2><a name="robotics">Robotics journals</a></h2><br/>
                 <ul>
-                <li><a href="">International Journal of Robotic Research</a></li>
+                    <li><a href='http://www.ijrr.org/'>International Journal of Robotic Research</a></li>
                 </ul>
             </row>
             <row class="row">
                 <h2><a name="statistics">Statistics journals</a></h2><br/>
                 <ul>
-                <li><a href="">Journal of Statistical Software</a></li>
+                    <li><a href='http://www.jstatsoft.org/'>Journal of Statistical Software</a></li>
                 </ul>
             </row>
         </div> <!-- /service -->
